e9b549abc3f75546a72b5c3288ee58da532cbc0c
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-07-20 15:01:54 +0900

    TestControllerのコミット

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/test', [TestController::class, 'test'])->name('test');
